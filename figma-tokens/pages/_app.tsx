645486f0abb4f912c72aece8b0bc0fbfba59c876
Paul Hammond <paul.hammond@gmail.com>
2022-11-28 10:21:32 +0000

    Initial chakra setup

--- figma-tokens/pages/_app.tsx
+++ figma-tokens/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+export const theme = extendTheme({ colors });
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
